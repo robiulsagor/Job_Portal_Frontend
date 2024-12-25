dd0c3135e8e2dad266e9b26db54ff5434f222920
robiulsagor <robiul100.me@gmail.com>
2024-12-25 19:15:48 +0600

    Frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <h2>Hi tere</h2>
+    </div>
+  );
+}
+
+export default App;
